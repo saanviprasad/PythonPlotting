b5c6938a2c4b75124e8791e5a534d874f0518584
torococo <rafaelrbravo@gmail.com>
2022-09-11 18:49:14 -0400

    fixed line plot import

--- SingleVoxel 3a Simulation/Single Voxel Line Plotting.py
+++ SingleVoxel 3a Simulation/Single Voxel Line Plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from tabulate import tabulate
 import seaborn as sns
 import pandas as pd
-
+from LinePlot import PopulationLinePlot
 
 SENSITIVE = 1
 RESISTANT = 0
@@ -18,7 +18,8 @@ Init_Res_Populations = [250, 500, 750, 2500, 5000, 7500, 25000, 50000, 75000]
 Init_Sens_Populations = [249750, 499500, 749250, 247500, 495000, 742500, 225000, 450000, 675000]
 RESPOP = 75000
 SENSPOP = 675000
-SingleVoxelData = np.loadtxt(fr"C:\Users\Administrator\PycharmProjects\pythonProject2\SingleVoxel 3a Simulation\SingleVoxelAdaptiveTherapy_0.5density_nocost_{SENSPOP}initsensitive_{RESPOP}initresistant1.csv", delimiter=",")
+#SingleVoxelData = np.loadtxt(fr"C:\Users\Administrator\PycharmProjects\pythonProject2\SingleVoxel 3a Simulation\SingleVoxelAdaptiveTherapy_0.5density_nocost_{SENSPOP}initsensitive_{RESPOP}initresistant1.csv", delimiter=",")
+SingleVoxelData = np.loadtxt(fr"SingleVoxelAdaptiveTherapy_0.5density_nocost_{SENSPOP}initsensitive_{RESPOP}initresistant1.csv", delimiter=",")
 data.append(SingleVoxelData)
 for i,f in enumerate(data):
     PopulationLinePlot(f[RESISTANT], "Resistant Cells")
